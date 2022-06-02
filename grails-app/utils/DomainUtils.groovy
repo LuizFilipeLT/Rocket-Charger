a34fe67216d8e4d8adf9f50cb62c83acc24a3ca9
Luiz Filipe Lima Teston <luizfilipe.liima@gmail.com>
2022-06-02 15:56:43 -0300

    arquivos para inicio de validação back.

--- grails-app/utils/DomainUtils.groovy
+++ grails-app/utils/DomainUtils.groovy
@@ -3,7 +3,7 @@ package com.rocketcharger.utils
     class DomainUtils {
 
         public static Object addError(Object entity, String message) {
-        entity.errors.reject("", null, message)
+        entity.errors.reject("", null, "Ocorreu um erro, contate o administrador.")
         return entity
     }
-} 
+}
