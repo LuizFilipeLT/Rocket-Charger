525e1d9bc37463e3f0a526b59782529deb8f5564
Luiz Filipe Lima Teston <luizfilipe.liima@gmail.com>
2022-06-09 22:10:49 -0300

    .

--- grails-app/assets/javascripts/utils/FormUtilsController.js
+++ grails-app/assets/javascripts/utils/FormUtilsController.js
@@ -1,28 +1,8 @@
 function FormUtilsController() {
   this.init = function () {
-    bindFormSubmit();
     bindHideButtonAndEditForm();
     validateRequiredsInputs();
   };
-
-  function bindFormSubmit() {
-    $("form").on("submit", function (e) {
-      e.preventDefault();
-    });
-  }
-
-  function validateRequiredsInputs() {
-    let cpfCnpjValue = cpfCnpj.value;
-    validateName();
-    if (cpfCnpjValue.length == correctCpfLength) {
-      validateCpf();
-    } else {
-      validateCnpj();
-    }
-    validatePostal();
-    validateEmail();
-    validateForm();
-  }
 
   function bindHideButtonAndEditForm() {
     let inputsReferenceList = $("input");
